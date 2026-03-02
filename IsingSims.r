fcc563f85a8c8073272b276c02b4b5b3137c6ce8
wescabral <wesley_cabral_alves@hotmail.com>
2026-03-02 07:56:21 -0300

    adding MCML function

--- IsingSims.r
+++ IsingSims.r
@@ -1,15 +1,21 @@
+library(tictoc)
+tic()
+
 # Parâmetros do Modelo
 grid_size <- 50       # Tamanho da imagem (50x50)
 n_colors <- 3         # Cores
-alpha <- 0.6       # Negativo para ser valorizar vizinhos iguais
-betas <- c(0, 0, 0.7)   # Penalização para cada cor
+alpha <- -0.6       # Negativo para ser valorizar vizinhos iguais
+betas <- c(0, 0, 0.2)   # Penalização para cada cor
 n_iterations <- 500000 # Sugestão do livro
+
+
+### GERAÇÃO DA IMAGEM
 
 # Aleatorizando o grid
 grid <- matrix(sample(0:(n_colors-1), grid_size^2, replace = TRUE), nrow = grid_size)
 
 # Contagem de vizinhos iguais
-count_matches <- function(g, i, j, color) {
+count_matches_by_color <- function(g, i, j, color) {
   res <- 0
   if(i > 1) res <- res + (g[i-1, j] == color)
   if(i < grid_size) res <- res + (g[i+1, j] == color)
@@ -28,7 +34,7 @@ for(step in 1:n_iterations) {
   
   for(color in 0:(n_colors-1)) {
    # Calcula a energia de cada cor
-    energies[color + 1] <- -alpha * count_matches(grid, i, j, color) - betas[color + 1]
+    energies[color + 1] <- -alpha * count_matches_by_color(grid, i, j, color) - betas[color + 1]
   }
   
   # Transforma as energias em probabilidade
@@ -42,6 +48,8 @@ for(step in 1:n_iterations) {
 # Gráfico
 image(grid, col = topo.colors(n_colors), main = "Simulação MRF Multi-Cores")
 
+
+### PSEUDOVEROSSIMILHANÇA
 
 # Função de Log-PL que opera diretamente sobre a matriz 'grid'
 log_pl <- function(params) {
@@ -65,7 +73,7 @@ log_pl <- function(params) {
       color_obs <- grid[i, j]
       
       # Número de matches
-      n_viz_obs <- count_matches(grid, i, j, color_obs)
+      n_viz_obs <- count_matches_by_color(grid, i, j, color_obs)
       
       # Numerador
       num <- exp(-alpha_est * n_viz_obs - betas_est[color_obs + 1])
@@ -73,7 +81,7 @@ log_pl <- function(params) {
       # Denominador
       den <- 0
       for(m in 0:(n_colors-1)) {
-        n_viz_m <- count_matches(grid, i, j, m)
+        n_viz_m <- count_matches_by_color(grid, i, j, m)
         den <- den + exp(-alpha_est * n_viz_m - betas_est[m + 1])
       }
       
@@ -87,14 +95,88 @@ log_pl <- function(params) {
 }
 
 # Otimização
-fit <- optim(par = c(0.6, 0, 0.7), fn = log_pl)
+fit_pl <- optim(par = c(0, 0, 0), fn = log_pl)
 
-# Resultados
+
+### VEROSSIMILHANÇA VIA MONTE CARLO
+
+# Função para calcular as estatísticas suficientes t(omega)
+t_omega <- function(grid) {
+
+  # Contagem de matches independente da cor
+  matches_hor <- sum(grid[, -ncol(grid)] == grid[, -1])
+  matches_ver <- sum(grid[-nrow(grid), ] == grid[-1, ])
+
+  # Contagem de cada cor
+  color_counts <- as.vector(table(factor(grid, levels = 0:(n_colors-1))))
+  return(c(matches = matches_hor + matches_ver, counts = color_counts))
+}
+
+t_obs = t_omega(grid)
+
+# Parâmetros Psi obtidos através da pseudoverossimilhança
+psi_alpha <- fit_pl$par[1]
+psi_betas <- c(0, fit_pl$par[2], fit_pl$par[3]) # b0 fixo em 0 como referência
+
+n_samples <- 5000  # Quantidade de amostras para aproximar a verossimilhança
+t_omega_sims <- matrix(0, nrow = n_samples, ncol = 1 + n_colors)
+temp_grid <- grid
+
+for(s in 1:n_samples) {
+
+  # Realiza algumas iterações de Gibbs para cada amostra
+  for(rep in 1:5000) {
+
+    i <- sample(1:grid_size, 1) 
+    j <- sample(1:grid_size, 1)
+
+    energies_sim <- sapply(0:(n_colors-1), function(c) {
+      -psi_alpha * count_matches_by_color(temp_grid, i, j, c) - psi_betas[c + 1]
+    })
+
+    probs <- exp(energies_sim) / sum(exp(energies_sim))
+
+    temp_grid[i,j] <- sample(0:(n_colors-1), 1, prob = probs)
+  }
+
+  t_omega_sims[s, ] <- t_omega(temp_grid)
+}
+
+# Função de log-verossimilhança via Monte Carlo
+log_mcml <- function(params) {
+  alpha_est <- params[1]
+  betas_est <- c(0, params[2], params[3])
+  
+  # Energia da imagem observada sob o novo parâmetro theta
+  u_theta_obs <- -alpha_est * t_obs[1] - sum(betas_est * t_obs[2:4])
+  
+  # Diferença de energia para as amostras simuladas
+  diff_alpha <- alpha_est - psi_alpha
+  diff_betas <- betas_est - psi_betas
+  
+  # Diferença u_diff para cada amostra simulada
+  u_diffs <- -diff_alpha * t_omega_sims[, 1] - (t_omega_sims[, 2:4] %*% diff_betas)
+  
+  # Log-verossimilhança
+  log_ratio_Z <- log(mean(exp(u_diffs)))
+  l_theta <- u_theta_obs - log_ratio_Z
+  
+  return(-l_theta) # Negativo para minimizar
+}
+
+# Otimização final MCML
+fit_mcml <- optim(par = fit_pl$par, fn = log_mcml)
+
+
+### RESULTADOS
 
 results <- data.frame(
   Parâmetro = c("Alpha", "Beta Ref", "Beta 1", "Beta 2"),
   Real = c(alpha, betas),
-  Estimado = c(fit$par[1], "-", fit$par[2], fit$par[3])
+  PL = c(fit_pl$par[1], "-", fit_pl$par[2], fit_pl$par[3]),
+  MCML = c(fit_mcml$par[1], "-", fit_mcml$par[2], fit_mcml$par[3])
 )
 
-results
+print(results)
+
+toc()
