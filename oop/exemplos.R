9a0a2ea20e572f90524d269a901d7626a59371f7
Freguglia <victorfreguglia@gmail.com>
2026-08-10 16:36:55 -0300

    Versão oculta primeira.

--- oop/exemplos.R
+++ oop/exemplos.R
@@ -12,21 +12,22 @@ source("./bayesianInference.R")
 # 1. Modelo verdadeiro e imagem simulada
 
 seeds_true <- list(
-  list(position = c(50, 50),   color = 1, delta = 10),
-  list(position = c(100, 100), color = 2, delta = 20)
+  list(position = c(50, 50),   color = 1, delta = 20),
+  list(position = c(100, 100), color = 1, delta = 20)
 )
 
-mod <- seedModel$new(alpha = 1.2, beta = 1, seeds = seeds_true, ncolors = 3)
-img <- mod$sampleImage(dim = c(150, 150), steps = 80)
+mod <- seedModel$new(alpha = 1.5, beta = 1, seeds = seeds_true, ncolors = 2)
+#img <- mod$sampleImage(dim = c(150, 150), steps = 80)
+fit <- fit_ghm(Y, mrfi(1), theta = expand_array(0, "onepar", mrfi(1), 1))
+img <- imageData$new(fit$Z_pred)
 
 # 2. Máxima pseudo-likelihood
 
 seeds_info <- list(
-  list(color = 1, n_seeds = 1),
-  list(color = 2, n_seeds = 1)
+  list(color = 1, n_seeds = 2)
 )
 
-fitter <- seedModelFitter$new(image = img, seeds_info = seeds_info, ncolors = 3)
+fitter <- seedModelFitter$new(image = img, seeds_info = seeds_info, ncolors = 2)
 mle    <- fitter$fit(alpha_init = 1, beta_init = 1)
 
 mle              # estimativas + convergência
@@ -41,22 +42,23 @@ mle$model$sampleImage(c(150, 150), steps = 80)$plot()
 
 prioris <- list(
   shape_alpha = 2, rate_alpha = 1, 
-  shape_beta  = 2, rate_beta  = 1,
-  shape_delta = 10, rate_delta = 1
+  shape_beta  = 5, rate_beta  = 0.5,
+  shape_delta = 5, rate_delta = 0.5
 )
 
-sampler <- seedBayesian$new(image = img, seeds_info = seeds_info, ncolors = 3, priors = prioris)
+sampler <- seedBayesian$new(image = img, seeds_info = seeds_info, ncolors = 2, priors = prioris)
 sampler   # estrutura e hiperparâmetros
 
 # init = NULL: inicializa automaticamente no MLE
-res <- sampler$run(n_iter = 5000, step_size = 0.05)
+res <- sampler$run(n_iter = 5000, step_size = 0.1)
 
 res$plot_logpl_trace()          # diagnóstico de convergência
 res$plot_image(n_samples = 300, burn_in = 3000) # imagem + 300 círculos (centros variáveis)
 
 # Burn-in manual
-res$plot_traces(burn_in = 3000)
+res$plot_traces(burn_in = 000)
 res$plot_densities(burn_in = 3000)
 print(res, burn_in = 3000)
 res$posterior_mean(burn_in = 3000)
 res$credible_interval(level = 0.95, burn_in = 3000)
+res$plot_positions()
