eab1627b669a37f5351f37611e08c85ce11f977e
Wesley <56455532+wescabral@users.noreply.github.com>
2026-08-25 15:42:34 -0300

    Update bayesianInference.Ratualizando log_post com o novo Z

--- oop/bayesianInference.R
+++ oop/bayesianInference.R
@@ -711,6 +711,8 @@ seedBayesianCont <- R6Class(
           )
           private$.mus <- noise_params$mus
           private$.sigmas <- noise_params$sigmas
+
+          log_post <- private$log_posterior(par, distMatrix)
         }
 
         if (i %% update_interval == 0L) setTxtProgressBar(pb, i)
